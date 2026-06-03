467acfd8b3bdfcb1c8f6cdc6d356ce2e06af145a
Mdjahidul Islam <mjislam6@myseneca.ca>
2026-06-03 18:36:41 -0400

    Added lab2g

---
+++ lab2g.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Author:Md Jahidul Islam
+# Author ID: mjislam6
+# Date Created: 2026/06/03
+
+import sys
+
+if len(sys.argv) == 2:
+    timer = int(sys.argv[1])
+else:
+    timer = 3
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
