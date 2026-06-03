a7461dc261407b283728170d1f7d23b987a24933
Mdjahidul Islam <mjislam6@myseneca.ca>
2026-06-03 18:33:41 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Author:Md Jahidul Islam
+# Author ID: mjislam6
+# Date Created: 2026/06/03
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
