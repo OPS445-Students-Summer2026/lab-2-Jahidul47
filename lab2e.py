4b0a8bcac758c6a13ae2908f9a7f95f3399d879b
Mdjahidul Islam <mjislam6@myseneca.ca>
2026-06-03 18:29:53 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Md Jahidul Islam
+# Author ID: mjislam6
+# Date Created: 2026/06/03
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print("blast off!")
